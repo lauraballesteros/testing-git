d7ba40fa2b4835bc5e0f93069895d174f819568f
Laura Nicolle Ballesteros Tellez <u1201749@unimilitar.edu.co>
2020-03-15 23:23:14 -0500

    python study part 1

---
+++ strings.py
@@ -0,0 +1,42 @@
+""" # asignacion
+mensaje= "hola"
+mensaje+= " "
+mensaje+= "Laura"
+print(mensaje) """
+
+# concatenacion
+""" messa1= "I'm"
+messa2= " "
+messa3= "love"
+print(messa1+messa2+messa3)
+result=messa1+messa2+messa3
+print(result)
+
+num_1= 10
+num_2= 5
+result2= num_1+num_2
+result2= str(result2)
+print("El resultado de la suma es:"+ result2) """
+
+# busqueda
+""" mensaje="Im freaking nervous about this interview"
+buscar_subcadena= mensaje.find("about")
+print(buscar_subcadena) """
+
+# extraccion
+""" mensaje= "Im in love with Harry, he is the cutest puppy ever"
+buscar_subcadena=mensaje.find("Harry")
+start= buscar_subcadena
+end= buscar_subcadena+5
+extraer_subcadena= mensaje[start:end]
+print(extraer_subcadena)
+ """
+
+# comparacion
+mensaje1="hola"
+mensaje2="hola"
+mensaje3="HOLA "
+mensaje4=" HOLA"
+
+print(mensaje1==mensaje2)
+print(mensaje3==mensaje4)
